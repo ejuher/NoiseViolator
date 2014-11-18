fa7d7d88329ad9494ca09df39b29f6c442916d02
Erich Uher <ejuher@gmail.com>
2014-11-18 11:58:13 -0800

    initial commit

---
+++ js/application.js
@@ -0,0 +1,11 @@
+window.NoiseViolator = {
+	Models: {},
+	Collections: {},
+	Views: {},
+	Routers: {},
+	initialize: function() {
+ 		Backbone.history.start();
+ 	}
+};
+
+$(NoiseViolator.initialize());
